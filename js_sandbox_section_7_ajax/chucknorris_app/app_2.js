d188e157620cdbf14f67d54eadc62cceef4b794e
Stephen Schuez <stephen.schuez@gmail.com>
2021-02-03 11:11:55 +0100

    did second chuck norris app by myself

--- js_sandbox_section_7_ajax/chucknorris_app/app_2.js
+++ js_sandbox_section_7_ajax/chucknorris_app/app_2.js
@@ -1,8 +1,11 @@
-document.querySelector('.get-jokes').addEventListener('click', getJokes);
+// UI vars 
+const button = document.querySelector('.get-jokes');
+
+button.addEventListener('click', getJokes);
 
 function getJokes(e){
-  const number = document.querySelector('input[type="number"]').value;
-
+  const number = document.getElementById('number').value;
+  
   const xhr = new XMLHttpRequest();
 
   xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
@@ -12,17 +15,16 @@ function getJokes(e){
   xhr.onload = function(){
     if(this.status === 200){
       const response = JSON.parse(this.responseText);
-      
+
       let output = '';
 
       if(response.type === 'success'){
         response.value.forEach(function(joke){
-          output += `<li>${joke.joke}</li>`;
-        });
+          output += `<li>${joke.joke}</li>`; 
+        })
       } else {
-        output += '<li>Something went wrong</li>';
+        return `<li>Something went wrong</li>`;
       }
-
       document.querySelector('.jokes').innerHTML = output;
     }
   }
