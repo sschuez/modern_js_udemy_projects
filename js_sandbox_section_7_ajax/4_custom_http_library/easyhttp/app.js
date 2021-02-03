233641da89d31cbcc3a0c74406c5ef993abfc938
Stephen Schuez <stephen.schuez@gmail.com>
2021-02-03 15:13:27 +0100

    Covered easy HTTP

--- js_sandbox_section_7_ajax/4_custom_http_library/easyhttp/app.js
+++ js_sandbox_section_7_ajax/4_custom_http_library/easyhttp/app.js
@@ -1,4 +1,85 @@
 const http = new easyHTTP;
 
 // Get Posts
-http.get('http://jsonplaceholder.typicode.com/posts');
+// http.get('http://jsonplaceholder.typicode.com/posts', function (err, response){
+// 	if(err){
+// 		console.log(err);
+// 	} else {
+// 		console.log(response);
+// 	}
+
+// 	console.log(response);
+// });
+
+
+// Get single Post
+// http.get('http://jsonplaceholder.typicode.com/posts/1', function (err, post){
+// 	if(err){
+// 		console.log(err);
+// 	} else {
+// 		console.log(post);
+// 	}
+
+// 	console.log(response);
+// });
+
+
+// Post Post
+// Create Data
+const data = {
+	title: 'Custom Post',
+	body: 'This is a custom post'
+};
+
+// Create Post
+// http.post('http://jsonplaceholder.typicode.com/posts', data, function(err, post){
+// 		if(err){
+// 			console.log(err);
+// 		} else {
+// 			console.log(post);
+// 		}
+// });
+
+// Update Post
+// http.put('http://jsonplaceholder.typicode.com/posts/1', data, function(err, post){
+// 		if(err){
+// 			console.log(err);
+// 		} else {
+// 			console.log(post);
+// 		}
+// });
+
+// Delete Post
+http.delete('http://jsonplaceholder.typicode.com/posts/1', function (err, response){
+	if(err){
+		console.log(err);
+	} else {
+		console.log(response);
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
