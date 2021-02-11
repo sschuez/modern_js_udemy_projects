0f7c2d831f62c518ab30a77dd15da1b21ace3f6e
Stephen Schuez <stephen.schuez@gmail.com>
2021-02-11 11:44:53 +0100

    finished github finder project with github API

--- githubfinder/github.js
+++ githubfinder/github.js
@@ -1,7 +1,7 @@
 class Github {
   constructor() {
-    this.client_id = 'd9308aacf8b204d361fd';
-    this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
+    this.client_id = '84d0bfabd0e52fda917b';
+    this.client_secret = '8a927c2c2f8078ffdef4402686fc305860c89db7';
     this.repos_count = 5;
     this.repos_sort = 'created: asc';
   }
--- githubfinder_project/github.js
+++ githubfinder_project/github.js
@@ -2,15 +2,21 @@ class GitHub {
 	constructor(){
 		this.client_id = '84d0bfabd0e52fda917b';
 		this.client_secret = 'e7af133fc2c1aea4acd8b0a5e21cdeab2b085bdf'
+		this.repos_count = 5;
+		this.repos_sort = 'created: asc'
 	}
 
 	async getUser(user){
 		const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+		const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
 		const profile = await profileResponse.json();
+		const repos = await repoResponse.json();
 
 		return {
-			profile
+			profile,
+			repos
 		}
 	}
 }
